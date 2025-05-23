06e8326e41b41843b5e5912b1349e4f1a091de95
Arjun Shukla <arjunshukla22@gmail.com>
2025-05-23 19:46:54 +0530

    Initial Commit

---
+++ ECommerceProductBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ECommerceProductBrowser
+//
+//  Created by Arjun Shukla on 23/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ECommerceProductBrowserTests/ECommerceProductBrowserTests.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceProductBrowserTests.swift
+//  ECommerceProductBrowserTests
+//
+//  Created by Arjun Shukla on 23/05/25.
+//
+
+import Testing
+@testable import ECommerceProductBrowser
+
+struct ECommerceProductBrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
